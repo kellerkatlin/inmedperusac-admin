f7d58f799f746882adc7085c51da649c57b27fa3
kellerkatlin <kellerkatlin.k@gmail.com>
2025-09-02 12:15:12 -0500

    fix

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,4 @@
 export const environment = {
-    production: false,
-    apiBaseUrl: 'https://catalog.arianjosafat.com/api',
-    keycloak: {
-        url: 'https://keycloak.moccapp.site/realms/moccapp-realm-dev/protocol/openid-connect/token',
-        client_id: 'spring-client-api-rest',
-        client_secret: '1nl5xx9nlG6vC2mDtfAK9ENI9uVgPfqs',
-        grant_type: 'password'
-    }
+    production: true,
+    apiBaseUrl: 'https://catalog.arianjosafat.com/api'
 };
